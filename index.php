3981ceef1217d80eb9dd06ad4b6a1f37b8586b1e
maulianSp <maulian.saputra@Gmail.com>
2024-06-14 15:08:32 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistem Informasi Mahasiswa</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/all.css">
+</head>
+<body>
+    
+
+    <script src="js/bootstrap.js"></script>
+    <script src="js/all.js"></script>
+</body>
+</html>
